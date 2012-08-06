c4b80184d7ef380238da4ac81fcb324cd023787b
Mathias Pürzl <kontakt@dasgutezeichen.de>
2012-08-06 16:19:44 +0200

    Textausrichtung der Tooltips implementiert

--- public/javascripts/escience.js
+++ public/javascripts/escience.js
@@ -1,4 +1,18 @@
 // ToolTip - fügt neue Hiweisboxen mittels qTip2 hinzu
-function addToolTip($class, $text) {
-  $j($class).qtip({content: {text: $text}, style: {classes: 'ui-tooltip-shadow ui-tooltip-green'}, position: {at:'right center', my:'left center'}});
+function addToolTip() {
+  $selector = arguments[0];
+  $tooltip = arguments[1];
+  
+  $position = 'right';
+  if (arguments.length == 3) {
+    $position = arguments[2];
+  }
+  switch ($position) {
+    case 'right': $direction = 'left'; break;
+    case 'left': $direction = 'right'; break;
+    case 'top': $direction = 'bottom'; break;
+    case 'bottom': $direction = 'top'; break;
+  }
+  
+  $j($selector).qtip({content: {text: $tooltip}, style: {classes: 'ui-tooltip-shadow ui-tooltip-green'}, position: {at:''+$position+' center', my:''+$direction+' center'}});
 }
