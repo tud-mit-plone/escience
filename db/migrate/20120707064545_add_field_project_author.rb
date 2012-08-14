7ee86d942d5ccf4cced03ea2033174954dd0ead3
Mathias Pürzl <kontakt@dasgutezeichen.de>
2012-08-14 12:04:52 +0200

    Entfernen der Änderung zum Projekt (betreffs Löschen)

--- db/migrate/20120707064545_add_field_project_author.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldProjectAuthor < ActiveRecord::Migration
-  def self.up
-    add_column :projects, :author, :integer
-  end
-
-  def self.down
-    remove_column :projects, :author
-  end
-end
