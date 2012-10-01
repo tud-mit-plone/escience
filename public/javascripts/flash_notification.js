904cdb3ce4b380f73092972a01809a7cfb60a76d
Mathias Pürzl <Melmoth@Mathias.local>
2012-10-01 11:33:13 +0200

    Flash-Notification optisch aufbereitet

--- public/javascripts/flash_notification.js
+++ public/javascripts/flash_notification.js
@@ -1,25 +1,23 @@
 (function( jQuery, undefined ) {
    jQuery.notification = function(options) {
-	var opts = jQuery.extend({}, {type: 'notice', time: 3000}, options);
-        var o            = opts;
-        timeout          = setTimeout('jQuery.notification.removebar()', o.time);
-        var message_span = jQuery('<span />').addClass('jbar-content').html(o.message);
-        var wrap_bar     = jQuery('<div />').addClass('jbar jbar-top '+o.type).css("cursor", "pointer");
+	var opts = jQuery.extend({}, {type: 'notice', time: 5000}, options);
+      var o            = opts;
+      timeout          = setTimeout('jQuery.notification.removebar()', o.time);
+      var message_span = jQuery('<span />').addClass('jbar-content').html(o.message);
+      var wrap_bar     = jQuery('<div />').addClass('jbar jbar-top '+o.type).css("cursor", "pointer");
 
-	wrap_bar.click(function(){
-        	jQuery.notification.removebar()
-        });
+wrap_bar.click(function(){
+      	jQuery.notification.removebar()
+      });
 
-    	wrap_bar.append(message_span).hide()
-        	.insertBefore(jQuery('#container')).fadeIn('fast');
-    	};
+    	wrap_bar.append(message_span).hide().insertBefore(jQuery('#container')).slideDown(600);
+    };
 
     var timeout;
     jQuery.notification.removebar    = function(txt) {
         if(jQuery('.jbar-top').length){
             clearTimeout(timeout);
-
-            jQuery('.jbar-top').fadeOut('fast',function(){
+            jQuery('.jbar-top').slideUp('slow',function(){
                 jQuery(this).hide();
             });
         }   
