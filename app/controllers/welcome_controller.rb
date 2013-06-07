916f65445b30bad1187eb29eb4232eb641644a76
Christian Reichmann <creichma@imn.htwk-leipzig.de>
2013-06-07 15:22:16 +0200

    minor fixes: robots can't see anything ... i hope, errormsg for users, no jquery.qtip.min for base, fix for project index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -93,7 +93,7 @@ class WelcomeController < ApplicationController
 
 
   def robots
-    @projects = Project.all_public.active
+    #@projects = Project.all_public.active
     render :layout => false, :content_type => 'text/plain'
   end
 end
