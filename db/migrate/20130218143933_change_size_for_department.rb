fde497bb5ba061a6656b105df264b48ba84443ca
Christian Reichmann <creichma@imn.htwk-leipzig.de>
2013-06-26 11:25:39 +0200

    minor bugfixes

--- db/migrate/20130218143933_change_size_for_department.rb
+++ db/migrate/20130218143933_change_size_for_department.rb
@@ -1,9 +1,9 @@
 class ChangeSizeForDepartment < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE `redmine`.`users` CHANGE COLUMN `department` `department` VARCHAR(256) NOT NULL DEFAULT '';"
+    change_column('users','department', :string, :limit => 256) 
   end
 
   def self.down
-    execute "ALTER TABLE `redmine`.`users` CHANGE COLUMN `department` `department` VARCHAR(30) NOT NULL DEFAULT '';"
+    change_column('users','department', :string, :limit => 30)
   end
 end
