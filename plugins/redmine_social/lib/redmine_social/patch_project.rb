a0870d9cbcf96a3436446fcf002cee50b1e8da15
Christian Reichmann <creichma@imn.htwk-leipzig.de>
2013-07-05 13:34:01 +0200

    private project in my account patch

--- plugins/redmine_social/lib/redmine_social/patch_project.rb
+++ plugins/redmine_social/lib/redmine_social/patch_project.rb
@@ -16,4 +16,26 @@ module RedmineSocialExtends
         end
       end
     end
+    module ProjectsHelperExtension
+      module ClassMethods
+      end
+      
+      module InstanceMethods
+        def private_project_settings_tabs
+          tabs = [{:name => 'modules', :action => :select_project_modules, :partial => 'projects/settings/modules', :label => :label_module_plural},
+            {:name => 'wiki', :action => :manage_wiki, :partial => 'projects/settings/wiki', :label => :label_wiki},
+            {:name => 'repositories', :action => :manage_repository, :partial => 'projects/settings/repositories', :label => :label_repository_plural},
+            {:name => 'activities', :action => :manage_project_activities, :partial => 'projects/settings/activities', :label => :enumeration_activities}
+            ]
+        end
+      end
+      
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
+        receiver.class_eval do
+          
+        end
+      end
+    end
   end
