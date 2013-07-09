3fcee390c1e710c4e3d6e4cbd60645ab3b7e9777
Christian Reichmann <creichma@imn.htwk-leipzig.de>
2013-07-09 12:25:36 +0200

    redmine_social settings: now with select_tag support ;) yay

--- plugins/redmine_social/lib/redmine_social/patch_setting.rb
+++ plugins/redmine_social/lib/redmine_social/patch_setting.rb
@@ -3,12 +3,31 @@ module RedmineSocialExtends
     module ClassMethods
     end
     module InstanceMethods
+      def generate_select_from_key(key,value,name_pre,display_attribute="name")
+        keysplit = key.split('_')
+        begin 
+          return nil if keysplit.length < 2
+          obj = keysplit[keysplit.length-2].camelcase.constantize
+          if keysplit.last == 'id' && !obj.nil?
+            return select_tag("#{name_pre}[#{key}]",options_from_collection_for_select(obj.all, 'id', display_attribute,value.to_i))
+          end
+        rescue NameError => e
+        
+        end
+        return nil
+      end
+
       def settings_hash_to_html(settings,name_pre="settings")
         html = ""
         settings.each do |k,v| 
           unless v.class == Hash || v.class == HashWithIndifferentAccess
             html << "<label>#{name_pre.to_s.gsub("_"," ").gsub("["," ").gsub("]","=>")} #{k.to_s.gsub("_"," ")}</label><br />"
-            html << text_field_tag("#{name_pre}[#{k}]",v, :size => "#{v.length > 40 ? 80 : 40}")
+            selectable = generate_select_from_key(k,v,name_pre)
+            if !selectable.nil?
+              html << selectable
+            else
+              html << text_field_tag("#{name_pre}[#{k}]",v, :size => "#{v.length > 40 ? 80 : 40}")
+            end
             html << "<br />"
           else
             html << settings_hash_to_html(v,"#{name_pre}[#{k}]")
