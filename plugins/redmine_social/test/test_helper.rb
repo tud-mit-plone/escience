04e0ec12d7cb79b6b678a5956d0872ece6cf9007
Viktor Befort <v.befort@googlemail.com>
2014-10-01 19:01:22 +0200

    delete spec-Test

--- plugins/redmine_social/test/test_helper.rb
+++ plugins/redmine_social/test/test_helper.rb
@@ -1,2 +1,19 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+require "../init.rb"
+
+#require 'active_record'
+#require 'active_record/fixtures'
+ 
+#Fixtures.create_fixtures('./fixtures/', ActiveRecord::Base.connection.tables)
+
+
+# Ensure that we are using the temporary fixture path
+#class ActiveSupport::TestCase
+#  self.fixture_path = File.dirname(__FILE__) + '/../fixtures/'
+#
+#  def initialize
+#  	Rails.logger.info "----------->" + __FILE__
+#  	$stderr.puts "foo bar ---------000000"
+#  end
+#end
