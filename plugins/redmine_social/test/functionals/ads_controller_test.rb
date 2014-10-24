fdec72eb9f8bc72904834badb26690adb8714bcb
Oliver Skawronek <oskawron@imn.htwk-leipzig.de>
2014-10-24 18:23:37 +0200

    added use case and additional tests for new, show, edit, update and destroy action

--- plugins/redmine_social/test/functionals/ads_controller_test.rb
+++ plugins/redmine_social/test/functionals/ads_controller_test.rb
@@ -20,9 +20,18 @@ class AdsControllerTest < ActionController::TestCase
   end
   
   test "only admin can see index" do
+    # not logged in
+    User.current = nil
     get :index
     assert_response :redirect
     
+    # normal user logged in
+    normal_user = users(:users_002)
+    @request.session[:user_id] = normal_user.id
+    get :index
+    assert_response :forbidden
+    
+    # admin logged in
     admin = users(:users_001)
     @request.session[:user_id] = admin.id
     get :index
@@ -123,6 +132,105 @@ class AdsControllerTest < ActionController::TestCase
     result = assigns(:ads)
     assert_equal num_ads % per_page, result.all.count
   end
+  
+  test "only admin can show ad" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    # not logged in
+    User.current = nil
+    get :show, :id => ad.id
+    assert_response :redirect
+    
+    # normal user logged in
+    normal_user = users(:users_002)
+    @request.session[:user_id] = normal_user.id
+    get :show, :id => ad.id
+    assert_response :forbidden
+    
+    # admin logged in
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    get :show, :id => ad.id
+    assert_response :success
+  end
+  
+  test "show assigns correct ad and renders correct template" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    
+    get :show, :id => ad.id
+    assert_response :success
+    assert_equal ad.id, assigns(:ad).id
+    assert_template :show
+  end
+
+  test "only admin can get new ad" do
+    # not logged in
+    User.current = nil
+    get :new
+    assert_response :redirect
+    
+    # normal user logged in
+    normal_user = users(:users_002)
+    @request.session[:user_id] = normal_user.id
+    get :new
+    assert_response :forbidden
+    
+    # admin logged in
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    get :new
+    assert_response :success
+  end
+  
+  test "new assigns correct ad and renders correct template" do
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:ad)
+    assert_template :new
+  end
+
+  test "only admin can get edit ad" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    # not logged in
+    User.current = nil
+    get :edit, :id => ad.id
+    assert_response :redirect
+    
+    # normal user logged in
+    normal_user = users(:users_002)
+    @request.session[:user_id] = normal_user.id
+    get :edit, :id => ad.id
+    assert_response :forbidden
+    
+    # admin logged in
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    get :edit, :id => ad.id
+    assert_response :success
+  end
+  
+  test "edit assigns correct ad and renders correct template" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    
+    get :edit, :id => ad.id
+    assert_response :success
+    assert_equal ad.id, assigns(:ad).id
+    assert_template :edit
+  end
 
   test "creating an invalid ad redirects to new" do
     admin = users(:users_001)
@@ -155,13 +263,128 @@ class AdsControllerTest < ActionController::TestCase
       post :create, :ad => ad.attributes
     end
     
-    
     # test with login but not as admin
     normal_user = users(:users_005)
     @request.session[:user_id] = normal_user.id
     assert_no_difference 'Ad.count' do
       post :create, :ad => ad.attributes
     end
+  end
+  
+  test "only admin can update ad" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    # not logged in
+    User.current = nil
+    put :update, :id => ad.id, :ad => {:html => '<p>new html</p>'}
+    assert_response :redirect
+    
+    # normal user logged in
+    normal_user = users(:users_002)
+    @request.session[:user_id] = normal_user.id
+    put :update, :id => ad.id, :ad => {:html => '<p>new html</p>'}
+    assert_response :forbidden
+    
+    # admin logged in
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    put :update, :id => ad.id, :ad => {:html => '<p>new html</p>'}
+    assert_redirected_to ad_path(ad)
+  end
+  
+  test "update updates field" do
+    ad = create_dummy_ad()
+    ad.save!
+
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    put :update, :id => ad.id, :ad => {:html => '<p>new html</p>'}
+    ad.reload
+    assert_equal '<p>new html</p>', ad.html
+  end
+  
+  test "only admin can destroy ad" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    # not logged in
+    User.current = nil
+    delete :destroy, :id => ad.id
+    assert_response :redirect
+    
+    # normal user logged in
+    normal_user = users(:users_002)
+    @request.session[:user_id] = normal_user.id
+    delete :destroy, :id => ad.id
+    assert_response :forbidden
+    
+    # admin logged in
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    delete :destroy, :id => ad.id
+    assert_redirected_to ads_path
+  end
+  
+  test "destroy deletes correct record" do
+    ad = create_dummy_ad()
+    ad.save!
+    
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    
+    assert_difference 'Ad.count', -1 do
+      delete :destroy, :id => ad.id
+    end
+    
+    assert_equal 0, Ad.where(:id => ad.id).count, 'Ad is deleted'
+  end
+  
+  test "use case create update destroy ad" do
+    admin = users(:users_001)
+    @request.session[:user_id] = admin.id
+    
+    # new Ad
+    get :new
+    ad = assigns(:ad)
+    
+    # create Ad
+    ad.html = '<p>Lorem</p>'
+    ad.frequency = 1
+    ad.audience = 'all'
+    assert_difference 'Ad.count' do
+      post :create, :ad => ad.attributes
+    end
+    assert_not_nil assigns(:ad)
+    ad = assigns(:ad)
+    assert_redirected_to ad_path(ad)
+    
+    # follow redirection to show
+    get :show, :id => ad.id
+    assert_template :show
+    assert_equal ad.id, assigns(:ad).id
+    ad = assigns(:ad)
+    
+    # edit Ad
+    get :edit, :id => ad.id
+    assert_template :edit
+    assert_equal ad.id, assigns(:ad).id
+    ad.html = '<p>new html</p>'
+    ad.name = 'foo'
+    ad.location = 'bar'
+    
+    # update Ad
+    put :update, :id => ad.id, :ad => ad.attributes
+    assert_equal ad.id, assigns(:ad)
+    ad = assigns(:ad)
+    assert_redirected_to ad_path(ad)
+    assert_equal '<p>new html</p>', ad.html
+    
+    # destroy Ad
+    assert_difference 'Ad.count', -1 do
+      delete :destroy, :id => ad.id
+    end
+    assert_redirected_to ads_path
   end
   
   private
