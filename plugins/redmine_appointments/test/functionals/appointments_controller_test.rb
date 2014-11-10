b032e3a22f520186eb737b0ff50785d9309ae4bf
Viktor Befort <v.befort@googlemail.com>
2014-11-10 17:34:25 +0100

    #14 add functional-filter-test for Appointment

--- plugins/redmine_appointments/test/functionals/appointments_controller_test.rb
+++ plugins/redmine_appointments/test/functionals/appointments_controller_test.rb
@@ -16,10 +16,48 @@ class AppointmentsControllerTest < ActionController::TestCase
     DatabaseCleaner.clean
   end
 
-  test "filter find appointment" do
+  test "filter find_appointment" do
+    #wrong appointment id
+    get :show, :id => 1000050
+    assert_response :missing
 
+    #appointment is public
+    appointment = appointments(:appointments_001)
+    get :show, :id => appointment.id
+    assert_response :success
+    assert_template :show
   end
 
+  test "filter find_appointment with private appointment" do
+    #appointment is private
+    user = users(:users_002)
+    appointment1 = Appointment.create(:author_id => user.id, :subject => 'test',
+      :start_date => Date.new(2014,11), :is_private => true, :cycle => 0)
+    assert appointment1.save!
+    get :show, :id => appointment1.id
+    assert_response :redirect
+
+    #user is logged in
+    @request.session[:user_id] = user.id
+    User.current = user
+    get :show, :id => appointment1.id
+    assert_response :success
+    assert_template :show
+  end
+
+  test "test show if cycle not set" do
+    #TODO
+  end
+
+  test "index" do
+    get :index
+    assert_response :success
+    assert_template :index
+  end
+
+  test "create appointment" do
+    #post :create
+  end
 
   private
   # Because a bug, a overriden fixture_path method get not called for files,
