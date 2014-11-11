7bc933e3b8cee0ee6e030752e0d2089b9f62512f
Viktor Befort <v.befort@googlemail.com>
2014-11-11 13:57:07 +0100

    #14 more functional tests for Appointment

--- plugins/redmine_appointments/test/functionals/appointments_controller_test.rb
+++ plugins/redmine_appointments/test/functionals/appointments_controller_test.rb
@@ -29,8 +29,16 @@ class AppointmentsControllerTest < ActionController::TestCase
   end
 
   test "filter find_appointment with private appointment" do
-    #appointment is private
+    #Anonym user
     user = users(:users_002)
+    appointment1 = Appointment.create(:author_id => user.id, :subject => 'test',
+      :start_date => Date.new(2014,11), :is_private => true, :cycle => 0)
+    assert appointment1.save!
+    get :show, :id => appointment1.id
+    assert_response :redirect
+
+    #wrong user
+    @request.session[:user_id] = users(:users_003)
     appointment1 = Appointment.create(:author_id => user.id, :subject => 'test',
       :start_date => Date.new(2014,11), :is_private => true, :cycle => 0)
     assert appointment1.save!
@@ -55,9 +63,75 @@ class AppointmentsControllerTest < ActionController::TestCase
     assert_template :index
   end
 
-  test "create appointment" do
-    #post :create
+  test "create appointment if logged in" do
+    user = users(:users_002)
+    @request.session[:user_id] = user.id
+    post :create, :appointment =>{:subject => 'test',
+      :start_date => '11/01/2014', :start_time => Time.new.strftime("%H:%M:%S"),
+      :due_date => '11/02/2014', :due_time => Time.new.strftime("%H:%M:%S")}
+    assert_redirected_to appointment_path(assigns(:appointment).id)
+    assert_equal @controller.l(:notice_appointment_successful_create), flash[:notice]
+    assert_equal user.id, assigns(:appointment).author_id
+    #result.all.include?
+
+    #Wieviele werden nach dem create angezeigt? Seite wird nicht neugelande(?)
   end
+
+  test "create not valid appointment" do
+    user = users(:users_002)
+    @request.session[:user_id] = user.id
+    #start_date is after due_date
+    post :create, :appointment =>{:subject => 'test',
+      :start_date => '12/01/2014', :start_time => Time.new.strftime("%H:%M:%S"),
+      :due_date => '11/01/2014', :due_time => Time.new.strftime("%H:%M:%S")}
+    assert_equal @controller.l(:notice_appointment_fail_create), flash[:notice]
+    assert_redirected_to new_appointment_path
+  end
+
+  test "update appointment-cycle if logged in" do
+    user = users(:users_002)
+    @request.session[:user_id] = user.id
+    appointment = appointments(:appointments_001)
+    put :update, :id => appointment.id, :appointment =>{:subject => 'test',
+      :start_date => '11/14/2014', :start_time => '16:35:38',
+      :due_date => '12/14/2014', :due_time => '16:35:38', :cycle => 1}
+    assert_equal 1, assigns(:appointment).cycle
+  end
+
+  # test "update appointment as other user" do
+  #   user = users(:users_002)
+  #   current_user = users(:users_003)
+  #   @request.session[:user_id] = current_user.id
+  #   appointment = appointments(:appointments_001)
+  #   put :update, :id => appointment.id, :appointment =>{:subject => 'test',
+  #     :start_date => '11/14/2014', :start_time => '16:35:38',
+  #     :due_date => '12/14/2014', :due_time => '16:35:38', :cycle => 1}
+  #   assert_not_equal 1, assigns(:appointment).cycle
+  # end
+
+  test "destroy appointment if logged in" do
+    appointmentscount = (Appointment.getAllEventsWithCycle(Date.new(2014,11), Date.new(2015,1))).count - 1
+    user = users(:users_003)
+    @request.session[:user_id] = user.id
+    appointment = appointments(:appointments_003)
+    delete :destroy, :id => appointment.id
+    assert_equal appointmentscount, Appointment.getAllEventsWithCycle(Date.new(2014,11), Date.new(2015,1)).count
+  end
+
+  test "destroy private appointment as wrong user" do
+    user = users(:users_002)
+    appointment = Appointment.create(:author_id => user.id, :subject => 'test',
+      :start_date => Date.new(2014,11), :is_private => true, :cycle => 0)
+    assert appointment.save!
+    
+    current_user = users(:users_003)
+    @request.session[:user_id] = current_user
+    assert_difference '(Appointment.getAllEventsWithCycle(Date.new(2014,11), Date.new(2015,1))).count', 0 do
+      delete :destroy, :id => appointment.id
+    end
+  end
+
+  #TODO 2 Buttens werden fürs anlegen angezeigt
 
   private
   # Because a bug, a overriden fixture_path method get not called for files,
