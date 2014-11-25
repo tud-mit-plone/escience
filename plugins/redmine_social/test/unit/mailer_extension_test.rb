4213af1b08049e4596357bd6bfb23a3a4cc147cb
Viktor Befort <v.befort@googlemail.com>
2014-11-25 16:04:47 +0100

    #15 correct a unit test for MailerExtension

--- plugins/redmine_social/test/unit/mailer_extension_test.rb
+++ plugins/redmine_social/test/unit/mailer_extension_test.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../../../../test/test_helper', __FILE__)
 
 class MailerExtensionTest < ActiveSupport::TestCase
 
-  fixtures :users, :projects
+  fixtures :users
 
   def setup
     # track all changes during the test to rollback
@@ -16,15 +16,19 @@ class MailerExtensionTest < ActiveSupport::TestCase
   end
   
   test "send user_message" do
+    ActionMailer::Base.deliveries.clear
     author = users(:users_002)
     User.current = author
     to_user = users(:users_003)
-    user_message = UserMessage.create(:author => author, :user => author,
-      :subject => 'test Subject', :body => 'Test Message', :receiver => to_user, :parent => nil)
-    history = user_message.get_history
 
-    assert_equal 1, history.count
-    assert history.include?(user_message)
+    assert_difference 'ActionMailer::Base.deliveries.size' do
+      user_message = UserMessage.create(:author => author, :user => author,
+      :subject => 'test Subject', :body => 'Test Message', :receiver => to_user, :parent => nil)
+      
+      history = user_message.get_history
+      assert_equal 1, history.count
+      assert history.include?(user_message)
+    end
   end
 
 end
