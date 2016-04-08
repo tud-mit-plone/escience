b42e834cb86bba04a5ddbfc073d6acb97b1e9ea9
Sebastian Gottfried <sebastiangottfried@web.de>
2016-04-08 15:26:59 +0200

    Remove BigBlueButton Plugin
    Not needed anymore.

--- plugins/redmine_bbb/config/routes.rb
+++
@@ -1,7 +0,0 @@
-RedmineApp::Application.routes.draw do
-  match 'bbb', :controller => :bbb, :action => :start 
-  match 'bbb_return', :controller => :bbb, :action => :index 
-  match 'projects/:project_id/bbb/start_form', :controller => :bbb, :action => :start_form, :via => :POST 
-  match 'projects/:project_id/bbb/bbb_session', :controller => :bbb, :action => :bbb_session, :via => :GET
-  match 'projects/:project_id/bbb/bbb_session_files', :controller => :bbb, :action => :bbb_session_files, :via => :GET
-end
