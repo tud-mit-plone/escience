3d8c32b450911d6b69d4ebf83c4b1550d705f3d6
Sebastian Gottfried <sebastiangottfried@web.de>
2016-07-11 09:53:15 +0200

    DB Seeds: Create eScience Project
    It has a special role in the platform since its wiki stores the online
    documentation among other things.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@ Role.anonymous
 Role.non_member
 Role.member
 Role.owner
+
+Project.create :name => "eScience",
+               :description => "Das Hauptprojekt der eScience Forschungsplattform, dem sich jeder Benutzer der Community anschliesst",
+               :is_public => true,
+               :identifier => "escience",
+               :status => Project::STATUS_ACTIVE
