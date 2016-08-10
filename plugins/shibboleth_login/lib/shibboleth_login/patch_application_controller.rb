e885832b50e48d2ae8c5dcff37a6159454f2492d
Sebastian Gottfried <sebastiangottfried@web.de>
2016-08-10 10:49:41 +0200

    Shibboleth Login: Update User Data Periodically
     * Update properties sent by Shibboleth every 24 hours. This way name
       changes in the central IDM etc. take effect after some time.
    
     * Update last login date every 24 hours to allow activity tracking.

--- plugins/shibboleth_login/lib/shibboleth_login/patch_application_controller.rb
+++ plugins/shibboleth_login/lib/shibboleth_login/patch_application_controller.rb
@@ -9,26 +9,33 @@ module ShibbolethLoginExtends
         end
         user = nil
         uid = request.headers["Uid"]
+        gender_map = {
+          "1" => 'male',
+          "2" => 'female',
+        }
+        user_data = {
+          :firstname => request.headers["Cn"],
+          :lastname => request.headers["Sn"],
+          :mail => request.headers["mail"],
+          :confirm => true,
+          :salutation => gender_map[request.headers["gender"]] || "",
+        }
         unless uid.nil?
           user = User.active.find_by_login(uid) rescue nil
           if user.nil?
-            gender_map = {
-              "1" => 'male',
-              "2" => 'female',
-            }
-            user_data = {
-              :firstname => request.headers["Cn"],
-              :lastname => request.headers["Sn"],
-              :mail => request.headers["mail"],
-              :confirm => true,
-              :salutation => gender_map[request.headers["gender"]] || "",
-            }
             user = User.new(user_data)
             user.login = uid
             user.password = SecureRandom.hex(16)
             user.activate!
             user.save!
             user.reload
+          # Shibboleth login is stateless, we have to cheat a bit: update
+          # last login every 24 hours if the user is seen
+          elsif user.updated_on < 1.day.ago
+            user.update(user_data)
+          end
+          if user.last_login_on.nil? || user.last_login_on < 1.day.ago
+            user.update_attribute(:last_login_on, Time.now)
           end
         end
         user
