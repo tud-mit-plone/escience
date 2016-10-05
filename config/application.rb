0ce63656247e5e2c95abb0f3356e9d0fcae9fdc7
Sebastian Gottfried <sebastiangottfried@web.de>
2016-10-05 15:40:32 +0200

    Remove Byebug Invocation

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,6 @@ module RedmineApp
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
-        byebug
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
