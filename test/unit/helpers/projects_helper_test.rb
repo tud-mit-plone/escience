28548662d0a83821b067ee10b006cb762d546656
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2016-12-01 15:37:41 +0100

    Fix Projects Helper Unit Tests

--- test/unit/helpers/projects_helper_test.rb
+++ test/unit/helpers/projects_helper_test.rb
@@ -41,16 +41,16 @@ class ProjectsHelperTest < ActionView::TestCase
   def test_link_to_version_within_project
     @project = Project.find(2)
     User.current = User.find(1)
-    assert_equal '<a href="/versions/5">Alpha</a>', link_to_version(Version.find(5))
+    assert_equal '<a href="/versions/5">Alpha</a>', link_to_version(::Version.find(5))
   end
 
   def test_link_to_version
     User.current = User.find(1)
-    assert_equal '<a href="/versions/5">OnlineStore - Alpha</a>', link_to_version(Version.find(5))
+    assert_equal '<a href="/versions/5">OnlineStore - Alpha</a>', link_to_version(::Version.find(5))
   end
 
   def test_link_to_private_version
-    assert_equal 'OnlineStore - Alpha', link_to_version(Version.find(5))
+    assert_equal 'OnlineStore - Alpha', link_to_version(::Version.find(5))
   end
 
   def test_link_to_version_invalid_version
@@ -59,19 +59,19 @@ class ProjectsHelperTest < ActionView::TestCase
 
   def test_format_version_name_within_project
     @project = Project.find(1)
-    assert_equal "0.1", format_version_name(Version.find(1))
+    assert_equal "0.1", format_version_name(::Version.find(1))
   end
 
   def test_format_version_name
-    assert_equal "eCookbook - 0.1", format_version_name(Version.find(1))
+    assert_equal "eCookbook - 0.1", format_version_name(::Version.find(1))
   end
 
   def test_format_version_name_for_system_version
-    assert_equal "OnlineStore - Systemwide visible version", format_version_name(Version.find(7))
+    assert_equal "OnlineStore - Systemwide visible version", format_version_name(::Version.find(7))
   end
 
   def test_version_options_for_select_with_no_versions
     assert_equal '', version_options_for_select([])
-    assert_equal '', version_options_for_select([], Version.find(1))
+    assert_equal '', version_options_for_select([], ::Version.find(1))
   end
 end
