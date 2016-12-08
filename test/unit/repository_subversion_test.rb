d9e46c63f81ad1208e0ae0500d910936cdb61c81
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2016-12-08 14:37:33 +0100

    Fix Subversion Repo Unit Tests

--- test/unit/repository_subversion_test.rb
+++ test/unit/repository_subversion_test.rb
@@ -21,211 +21,218 @@ class RepositorySubversionTest < ActiveSupport::TestCase
   fixtures :projects, :repositories, :enabled_modules, :users, :roles
 
   NUM_REV = 11
+  REPOSITORY_ARCHIVE_PATH = Rails.root.join('test/fixtures/repositories/subversion_repository.dump').to_s
+  REPOSITORY_PATH = Rails.root.join('tmp/test/subversion_repository').to_s
+  REPOSITORY_URL = "file://#{REPOSITORY_PATH}"
 
   def setup
+    system "svnadmin create  \"#{REPOSITORY_PATH}\"",
+           :out => "/dev/null"
+    system "svnadmin load  \"#{REPOSITORY_PATH}\"",
+           :in => REPOSITORY_ARCHIVE_PATH,
+           :out => "/dev/null"
     @project = Project.find(3)
     @repository = Repository::Subversion.create(:project => @project,
-             :url => self.class.subversion_repository_url)
+             :url => REPOSITORY_URL)
     assert @repository
   end
 
-  if repository_configured?('subversion')
-    def test_fetch_changesets_from_scratch
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
+  def teardown
+    FileUtils.remove_dir REPOSITORY_PATH
+  end
 
-      assert_equal NUM_REV, @repository.changesets.count
-      assert_equal 20, @repository.filechanges.count
-      assert_equal 'Initial import.', @repository.changesets.find_by_revision('1').comments
+  def test_fetch_changesets_from_scratch
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+
+    assert_equal NUM_REV, @repository.changesets.count
+    assert_equal 20, @repository.filechanges.count
+    assert_equal 'Initial import.', @repository.changesets.find_by_revision('1').comments
+  end
+
+  def test_fetch_changesets_incremental
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+
+    # Remove changesets with revision > 5
+    @repository.changesets.find(:all).each {|c| c.destroy if c.revision.to_i > 5}
+    @project.reload
+    assert_equal 5, @repository.changesets.count
+
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+  end
+
+  def test_entries
+    entries = @repository.entries
+    assert_kind_of Redmine::Scm::Adapters::Entries, entries
+  end
+
+  def test_entries_for_invalid_path_should_return_nil
+    entries = @repository.entries('invalid_path')
+    assert_nil entries
+  end
+
+  def test_latest_changesets
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+
+    # with limit
+    changesets = @repository.latest_changesets('', nil, 2)
+    assert_equal 2, changesets.size
+    assert_equal @repository.latest_changesets('', nil).slice(0,2), changesets
+
+    # with path
+    changesets = @repository.latest_changesets('subversion_test/folder', nil)
+    assert_equal ["10", "9", "7", "6", "5", "2"], changesets.collect(&:revision)
+
+    # with path and revision
+    changesets = @repository.latest_changesets('subversion_test/folder', 8)
+    assert_equal ["7", "6", "5", "2"], changesets.collect(&:revision)
+  end
+
+  def test_directory_listing_with_square_brackets_in_path
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+
+    entries = @repository.entries('subversion_test/[folder_with_brackets]')
+    assert_not_nil entries, 'Expect to find entries in folder_with_brackets'
+    assert_equal 1, entries.size, 'Expect one entry in folder_with_brackets'
+    assert_equal 'README.txt', entries.first.name
+  end
+
+  def test_directory_listing_with_square_brackets_in_base
+    @project = Project.find(3)
+    @repository = Repository::Subversion.create(
+                        :project => @project,
+                        :url => "file:///#{REPOSITORY_PATH}/subversion_test/[folder_with_brackets]")
+
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+
+    assert_equal 1, @repository.changesets.count, 'Expected to see 1 revision'
+    assert_equal 2, @repository.filechanges.count, 'Expected to see 2 changes, dir add and file add'
+
+    entries = @repository.entries('')
+    assert_not_nil entries, 'Expect to find entries'
+    assert_equal 1, entries.size, 'Expect a single entry'
+    assert_equal 'README.txt', entries.first.name
+  end
+
+  def test_identifier
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    c = @repository.changesets.find_by_revision('1')
+    assert_equal c.revision, c.identifier
+  end
+
+  def test_find_changeset_by_empty_name
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    ['', ' ', nil].each do |r|
+      assert_nil @repository.find_changeset_by_name(r)
     end
+  end
 
-    def test_fetch_changesets_incremental
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
+  def test_identifier_nine_digit
+    c = Changeset.new(:repository => @repository, :committed_on => Time.now,
+                      :revision => '123456789', :comments => 'test')
+    assert_equal c.identifier, c.revision
+  end
 
-      # Remove changesets with revision > 5
-      @repository.changesets.find(:all).each {|c| c.destroy if c.revision.to_i > 5}
-      @project.reload
-      assert_equal 5, @repository.changesets.count
+  def test_format_identifier
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    c = @repository.changesets.find_by_revision('1')
+    assert_equal c.format_identifier, c.revision
+  end
 
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-    end
+  def test_format_identifier_nine_digit
+    c = Changeset.new(:repository => @repository, :committed_on => Time.now,
+                      :revision => '123456789', :comments => 'test')
+    assert_equal c.format_identifier, c.revision
+  end
 
-    def test_entries
-      entries = @repository.entries
-      assert_kind_of Redmine::Scm::Adapters::Entries, entries
-    end
+  def test_activities
+    c = Changeset.new(:repository => @repository, :committed_on => Time.now,
+                      :revision => '1', :comments => 'test')
+    assert c.event_title.include?('1:')
+    assert_equal '1', c.event_url[:rev]
+  end
 
-    def test_entries_for_invalid_path_should_return_nil
-      entries = @repository.entries('invalid_path')
-      assert_nil entries
-    end
+  def test_activities_nine_digit
+    c = Changeset.new(:repository => @repository, :committed_on => Time.now,
+                      :revision => '123456789', :comments => 'test')
+    assert c.event_title.include?('123456789:')
+    assert_equal '123456789', c.event_url[:rev]
+  end
 
-    def test_latest_changesets
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-
-      # with limit
-      changesets = @repository.latest_changesets('', nil, 2)
-      assert_equal 2, changesets.size
-      assert_equal @repository.latest_changesets('', nil).slice(0,2), changesets
-
-      # with path
-      changesets = @repository.latest_changesets('subversion_test/folder', nil)
-      assert_equal ["10", "9", "7", "6", "5", "2"], changesets.collect(&:revision)
-
-      # with path and revision
-      changesets = @repository.latest_changesets('subversion_test/folder', 8)
-      assert_equal ["7", "6", "5", "2"], changesets.collect(&:revision)
-    end
-
-    def test_directory_listing_with_square_brackets_in_path
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-
-      entries = @repository.entries('subversion_test/[folder_with_brackets]')
-      assert_not_nil entries, 'Expect to find entries in folder_with_brackets'
-      assert_equal 1, entries.size, 'Expect one entry in folder_with_brackets'
-      assert_equal 'README.txt', entries.first.name
-    end
-
-    def test_directory_listing_with_square_brackets_in_base
-      @project = Project.find(3)
-      @repository = Repository::Subversion.create(
-                          :project => @project,
-                          :url => "file:///#{self.class.repository_path('subversion')}/subversion_test/[folder_with_brackets]")
-
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-
-      assert_equal 1, @repository.changesets.count, 'Expected to see 1 revision'
-      assert_equal 2, @repository.filechanges.count, 'Expected to see 2 changes, dir add and file add'
-
-      entries = @repository.entries('')
-      assert_not_nil entries, 'Expect to find entries'
-      assert_equal 1, entries.size, 'Expect a single entry'
-      assert_equal 'README.txt', entries.first.name
-    end
-
-    def test_identifier
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      c = @repository.changesets.find_by_revision('1')
-      assert_equal c.revision, c.identifier
-    end
-
-    def test_find_changeset_by_empty_name
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      ['', ' ', nil].each do |r|
-        assert_nil @repository.find_changeset_by_name(r)
+  def test_log_encoding_ignore_setting
+    with_settings :commit_logs_encoding => 'windows-1252' do
+      s1 = "\xC2\x80"
+      s2 = "\xc3\x82\xc2\x80"
+      if s1.respond_to?(:force_encoding)
+        s1.force_encoding('ISO-8859-1')
+        s2.force_encoding('UTF-8')
+        assert_equal s1.encode('UTF-8'), s2
       end
+      c = Changeset.new(:repository => @repository,
+                        :comments   => s2,
+                        :revision   => '123',
+                        :committed_on => Time.now)
+      assert c.save
+      assert_equal s2, c.comments
     end
+  end
 
-    def test_identifier_nine_digit
-      c = Changeset.new(:repository => @repository, :committed_on => Time.now,
-                        :revision => '123456789', :comments => 'test')
-      assert_equal c.identifier, c.revision
-    end
+  def test_previous
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    changeset = @repository.find_changeset_by_name('3')
+    assert_equal @repository.find_changeset_by_name('2'), changeset.previous
+  end
 
-    def test_format_identifier
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      c = @repository.changesets.find_by_revision('1')
-      assert_equal c.format_identifier, c.revision
-    end
+  def test_previous_nil
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    changeset = @repository.find_changeset_by_name('1')
+    assert_nil changeset.previous
+  end
 
-    def test_format_identifier_nine_digit
-      c = Changeset.new(:repository => @repository, :committed_on => Time.now,
-                        :revision => '123456789', :comments => 'test')
-      assert_equal c.format_identifier, c.revision
-    end
+  def test_next
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    changeset = @repository.find_changeset_by_name('2')
+    assert_equal @repository.find_changeset_by_name('3'), changeset.next
+  end
 
-    def test_activities
-      c = Changeset.new(:repository => @repository, :committed_on => Time.now,
-                        :revision => '1', :comments => 'test')
-      assert c.event_title.include?('1:')
-      assert_equal '1', c.event_url[:rev]
-    end
-
-    def test_activities_nine_digit
-      c = Changeset.new(:repository => @repository, :committed_on => Time.now,
-                        :revision => '123456789', :comments => 'test')
-      assert c.event_title.include?('123456789:')
-      assert_equal '123456789', c.event_url[:rev]
-    end
-
-    def test_log_encoding_ignore_setting
-      with_settings :commit_logs_encoding => 'windows-1252' do
-        s1 = "\xC2\x80"
-        s2 = "\xc3\x82\xc2\x80"
-        if s1.respond_to?(:force_encoding)
-          s1.force_encoding('ISO-8859-1')
-          s2.force_encoding('UTF-8')
-          assert_equal s1.encode('UTF-8'), s2
-        end
-        c = Changeset.new(:repository => @repository,
-                          :comments   => s2,
-                          :revision   => '123',
-                          :committed_on => Time.now)
-        assert c.save
-        assert_equal s2, c.comments
-      end
-    end
-
-    def test_previous
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      changeset = @repository.find_changeset_by_name('3')
-      assert_equal @repository.find_changeset_by_name('2'), changeset.previous
-    end
-
-    def test_previous_nil
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      changeset = @repository.find_changeset_by_name('1')
-      assert_nil changeset.previous
-    end
-
-    def test_next
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      changeset = @repository.find_changeset_by_name('2')
-      assert_equal @repository.find_changeset_by_name('3'), changeset.next
-    end
-
-    def test_next_nil
-      assert_equal 0, @repository.changesets.count
-      @repository.fetch_changesets
-      @project.reload
-      assert_equal NUM_REV, @repository.changesets.count
-      changeset = @repository.find_changeset_by_name('11')
-      assert_nil changeset.next
-    end
-  else
-    puts "Subversion test repository NOT FOUND. Skipping unit tests !!!"
-    def test_fake; assert true end
+  def test_next_nil
+    assert_equal 0, @repository.changesets.count
+    @repository.fetch_changesets
+    @project.reload
+    assert_equal NUM_REV, @repository.changesets.count
+    changeset = @repository.find_changeset_by_name('11')
+    assert_nil changeset.next
   end
 end
